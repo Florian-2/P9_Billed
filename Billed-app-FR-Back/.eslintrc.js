966e7a8e124d8755b2c402f042e9804c85f54501
Florian <sicilia.florian02@gmail.com>
2023-04-08 17:02:36 +0200

    Correction : [Bug Hunt] - Bills

--- Billed-app-FR-Back/.eslintrc.js
+++ Billed-app-FR-Back/.eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
     ecmaVersion: 13,
   },
   rules: {
+    'linebreak-style': ['error', 'windows'],
   },
   ignorePatterns: ['tests', 'setupTests.js', '*.test.js', 'migrations', 'models'],
 };
